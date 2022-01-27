4ad48370a56d4aaa71aade2c363d9dfd33538503
aminbenmansour <amin.benmansour.10@gmail.com>
2022-01-27 19:31:37 +0100

    more convenient way for functional components

--- src/components/item/Item.tsx
+++ src/components/item/Item.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+
 import { Button } from '@mui/material';
 import { CartItemType } from '../../App';
 import { Wrapper } from '../../App.styles';
@@ -7,18 +9,18 @@ type Props = {
     handleAddToCart: (clickedItem: CartItemType) => void;
 }
 
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const Item: React.FC<Props> = ({ item, handleAddToCart }) => (
-    <Wrapper>
-        <img src={item.image} alt={item.title} />
-        <div>
-            <h3> {item.title} </h3>
-            <p> {item.description} </p>
-            <h4> ${item.price} </h4>
-        </div>
-        <Button onClick={() => handleAddToCart(item)}> Add item to cart </Button>
-    </Wrapper>
-);
+function Item({ item, handleAddToCart }: Props) {
+    return (
+        <Wrapper>
+            <img src={item.image} alt={item.title} />
+            <div>
+                <h3> {item.title} </h3>
+                <p> {item.description} </p>
+                <h4> ${item.price} </h4>
+            </div>
+            <Button onClick={() => handleAddToCart(item)}> Add item to cart </Button>
+        </Wrapper>
+    );
+}
 
 export default Item;

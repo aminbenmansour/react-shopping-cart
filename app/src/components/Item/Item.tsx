56d583f5736850f95e5e8c0093d426e8b01afa78
aminbenmansour <amin.benmansour.10@gmail.com>
2022-01-28 02:46:15 +0100

    moving application to subfolder

---
+++ app/src/components/Item/Item.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+
+import { Button } from '@mui/material';
+import { CartItemType } from '../../App';
+import { Wrapper } from './Item.style';
+
+type Props = {
+    item: CartItemType;
+    handleAddToCart: (clickedItem: CartItemType) => void;
+}
+
+function Item({ item, handleAddToCart }: Props) {
+    return (
+        <Wrapper>
+            <img src={item.image} alt={item.title} />
+            <div>
+                <h3> {item.title} </h3>
+                <p> {item.description} </p>
+                <h4> ${item.price} </h4>
+            </div>
+            <Button onClick={() => handleAddToCart(item)}> Add item to cart </Button>
+        </Wrapper>
+    );
+}
+
+export default Item;
